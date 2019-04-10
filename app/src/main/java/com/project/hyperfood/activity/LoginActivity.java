7ed60b332d5eac7121845a6d8234d01a91145f80
Santipong Jarat <santipongjarat@gmail.com>
2019-04-10 15:37:35 +0700

    update login page

--- app/src/main/java/com/project/hyperfood/activity/LoginActivity.java
+++ app/src/main/java/com/project/hyperfood/activity/LoginActivity.java
@@ -9,11 +9,21 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.project.hyperfood.R;
+import com.project.hyperfood.common.model.User;
+import com.project.hyperfood.common.preferences.HPF;
 import com.project.hyperfood.databinding.ActivityLoginBinding;
 import com.project.hyperfood.common.utils.EmailUtil;
 
+import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
+
+import static com.project.hyperfood.application.HyperFoodApplication.USER;
 
 public class LoginActivity extends AbstractActivity{
 
@@ -51,14 +61,39 @@ public class LoginActivity extends AbstractActivity{
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete( Task<AuthResult> task) {
-                        progressDialog.dismiss();
                         if (task.isSuccessful()) {
-                           openReportPage();
+                            getUser(mAuth.getUid());
                         }else {
+                            progressDialog.dismiss();
                             Toast.makeText(getContext(), R.string.validate_register, Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+    }
+
+    private void getUser(String uid){
+        DatabaseReference diseaseRef = FirebaseDatabase.getInstance().getReference(USER).child(uid);
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                progressDialog.dismiss();
+                User user = dataSnapshot.getValue(User.class);
+                if (user != null){
+                    HPF.getInstance().setUser(user);
+                    openReportPage();
+                }else {
+                    Toast.makeText(getContext(), R.string.validate_register, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                progressDialog.dismiss();
+                Toast.makeText(getContext(), R.string.validate_register, Toast.LENGTH_SHORT).show();
+            }
+
+        };
+        diseaseRef.addListenerForSingleValueEvent(valueEventListener);
     }
 
     private void openReportPage(){
