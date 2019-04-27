19306cdfcafeffdde00377c5eda384cef40739a3
Santipong <santipongjarat@gmail.com>
2019-04-27 19:57:51 +0700

    clear shared preference on sign out

--- app/src/main/java/com/project/hyperfood/activity/SettingActivity.java
+++ app/src/main/java/com/project/hyperfood/activity/SettingActivity.java
@@ -1,6 +1,7 @@
 package com.project.hyperfood.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -12,20 +13,25 @@ import com.project.hyperfood.databinding.ActivitySettingsBinding;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 
+import static com.project.hyperfood.activity.ReportActivity.PREF_NAME;
+
 public class SettingActivity extends AbstractActivity{
 
     private ActivitySettingsBinding binding;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_settings);
+        preferences = getApplicationContext().getSharedPreferences(PREF_NAME, MODE_PRIVATE);
 
         binding.tvSetting.setTypeface(FontUtil.getFont(getAssets(), FontUtil.LAMMOON_BOLD));
         binding.btnBack.setOnClickListener(v -> onBackPressed());
         binding.btnSignOut.setOnClickListener(v ->{
             FirebaseAuth.getInstance().signOut();
             HPF.getInstance().clear();
+            preferences.edit().clear().apply();
             startActivity(new Intent(getContext(), SplashScreenActivity.class));
             finish();
         });
